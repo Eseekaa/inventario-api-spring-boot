f8fbdc00e0727b08ea60ff0c17c7609887a64f90
Matias Mercado <166638575+Eseekaa@users.noreply.github.com>
2025-06-06 02:43:13 -0400

    Add files via upload

---
+++ inventario-api-spring-boot/src/main/java/com/api/inventario_api_spring_boot/InventarioApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.api.inventario_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ inventario-api-spring-boot/src/test/java/com/api/inventario_api_spring_boot/InventarioApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.inventario_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
